1df38b8708826b86df9ffcf8550d5831ec63c086
Alan Newton <128156285+newtonalan@users.noreply.github.com>
2023-04-07 20:51:39 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+a = float(input("Digite a medida do lado A do triangulo: \n"))
+b = float(input("Digite a medida do lado B do triangulo: \n"))
+c = float(input("Digite a medida do lado C do triangulo: \n"))
+print(" ")
+
+if (a+b > c) and (a+c > b) and (b+c > a):
+  if (a == c) and (a == c) and (b == c):
+    print("Triângulo Equilátero")
+  elif (a != c) and (a != c) and (b != c):
+    print("Triângulo Escaleno")
+  else:
+    print("Triângulo Isósceles ")
+    
+else:
+  print("Não é um triângulo")
